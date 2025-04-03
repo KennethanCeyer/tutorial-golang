bdda17a01bfca533b5ce1b9d10fa82b0582a8af1
KennethanCeyer <kenneth@pigno.se>
2025-04-04 03:02:47 +0900

    feat: simplify arthmetic/inc_dec

--- ch05/operators/arithmetic/inc_dec/inc_dec.go
+++ ch05/operators/arithmetic/inc_dec/inc_dec.go
@@ -3,23 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	// 기존 방식
 	increase := 1
-	increase += 1
-	fmt.Println(increase) // 2
+	increase++ // increase = increase + 1과 같음
+	fmt.Println(increase)
 
-	// 증가 연산자
-	increaseSyntacticSugar := 1
-	increaseSyntacticSugar++
-	fmt.Println(increaseSyntacticSugar) // 2
-
-	// 기존 방식
 	decrease := 1
-	decrease -= 1
-	fmt.Println(decrease) // 0
-
-	// 감소 연산자
-	decreaseSyntacticSugar := 1
-	decreaseSyntacticSugar--
-	fmt.Println(decreaseSyntacticSugar) // 0
+	decrease-- // decrease = decrease - 1과 같음
+	fmt.Println(decrease)
 }
