4091cd61f546b645f799a16527d26dc876e235e7
KennethanCeyer <kenneth@pigno.se>
2025-04-04 06:50:50 +0900

    fix: range keyword example's reference issue

--- ch09/data_structures/basic_range/basic_range.go
+++ ch09/data_structures/basic_range/basic_range.go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {
 	score := make([]int, 5)
 
-	// 사용자로 부터 5 과목의 점수 입력
-	for i := 0; i < len(score); i++ {
+	// 사용자에게 5 과목의 점수 입력
+	for i := range score {
 		fmt.Printf("%d 번째 과목 점수를 입력하세요: ", i+1)
 		fmt.Scanf("%d", &score[i])
 	}
@@ -14,8 +14,8 @@ func main() {
 
 	// 모든 슬라이스 요소를 반복하며 평균 점수 계산
 	sumScore := 0
-	for i := 0; i < len(score); i++ {
-		sumScore += score[i]
+	for _, scoreItem := range score {
+		sumScore += scoreItem
 	}
 	meanScore := float64(sumScore) / float64(len(score))
 	fmt.Printf("당신의 점수 평균은 %.2f입니다.\n", meanScore)
